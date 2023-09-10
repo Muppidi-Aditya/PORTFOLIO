3703b5b78acca03c65c92aa68f123096dbae5c83
Muppidi-Aditya <112880727+Muppidi-Aditya@users.noreply.github.com>
2023-09-10 22:50:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let cursor = document.getElementById("cursor");
+
+document.addEventListener("mousemove",(e) => {
+    let x = e.pageX;
+    let y = e.pageY;
+
+    cursor.style.top = y + "px";
+    cursor.style.left = x + "px";
+});
